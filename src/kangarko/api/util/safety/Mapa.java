bff7b0c48058f25703ba8377b3ff07416090d23a
kangarko <kangaro.matej@azet.sk>
2014-11-03 19:22:13 +0100

    Trying to fix nosuchmethod error

--- src/kangarko/api/util/safety/Mapa.java
+++ src/kangarko/api/util/safety/Mapa.java
@@ -13,12 +13,13 @@ public class Mapa<Kluc, Hodnota> extends BezpecnostneVarovania {
 	public Hodnota put(Kluc key, Hodnota value) {
 		if (containsKey(key))
 			varovat("Map already contains key: " + key + " \\w value: " + value);
-		
+
 		return mapa.put(key, value);
 	}
-	
-	public Hodnota putIfAbsent(Kluc key, Hodnota value) {
-		return mapa.putIfAbsent(key, value);
+
+	public void putIfAbsent(Kluc key, Hodnota value) {
+		if (!containsKey(key))
+			put(key, value);
 	}
 
 	public void clear() {
@@ -29,8 +30,8 @@ public class Mapa<Kluc, Hodnota> extends BezpecnostneVarovania {
 	public boolean containsKey(Kluc key) {
 		return mapa.containsKey(key);
 	}
-	
-	
+
+
 	@SuppressWarnings("unchecked")
 	/** Beware: Only use when storing String as a Key! */
 	public boolean containsKey(Player pl) {
@@ -41,10 +42,10 @@ public class Mapa<Kluc, Hodnota> extends BezpecnostneVarovania {
 	public Hodnota remove(Kluc key) {
 		if (!containsKey(key))
 			varovat("Cannot remove key: " + key + " since it isnt in the map!");
-		
+
 		return mapa.remove(key);
 	}
-	
+
 	public int size() {
 		return mapa.size();
 	}
@@ -52,16 +53,16 @@ public class Mapa<Kluc, Hodnota> extends BezpecnostneVarovania {
 	public Hodnota get(Kluc key) {
 		if(mapa.get(key) == null)
 			varovat("Trying to get key: " + key + " but map doesnt contains him!");
-			
+
 		return mapa.get(key);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	/** Beware: Only use when storing String as a Key! */
 	public Hodnota get(Player pl) {
 		return get((Kluc) pl.getName());
 	}
-	
+
 	public boolean isEmpty() {
 		return mapa.isEmpty();
 	}
@@ -73,7 +74,7 @@ public class Mapa<Kluc, Hodnota> extends BezpecnostneVarovania {
 	public Set<Kluc> keySet() {
 		return mapa.keySet();
 	}
-	
+
 	public void zobrazovavatVarovania() {
 		varovat = true;
 	}
