4cc8654c5051f548aa39e89afcbd07a56687c401
kangarko <kangaro.matej@azet.sk>
2015-01-02 10:57:03 +0100

    Fix NPE when Vault didn't find eco/chat plugins. Closes #56

--- src/kangarko/chatcontrol/hooks/VaultHook.java
+++ src/kangarko/chatcontrol/hooks/VaultHook.java
@@ -1,34 +1,52 @@
 package kangarko.chatcontrol.hooks;
 
+import kangarko.chatcontrol.utils.Common;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicesManager;
 
 public class VaultHook {
 
-	private final Chat chat;
-	private final Economy economy;
+	private Chat chat;
+	private Economy economy;
 
 	public VaultHook() {
 		ServicesManager services = Bukkit.getServicesManager();
 
-		chat = services.getRegistration(Chat.class).getProvider();
-		economy = services.getRegistration(Economy.class).getProvider();
+		RegisteredServiceProvider<Economy> economyProvider = services.getRegistration(net.milkbowl.vault.economy.Economy.class);		
+		if (economyProvider != null)
+			economy = economyProvider.getProvider();
+		else
+			Common.Log("Economy plugin not found");
+
+		RegisteredServiceProvider<Chat> chatProvider = services.getRegistration(net.milkbowl.vault.chat.Chat.class);		
+		if (chatProvider != null)
+			chat = chatProvider.getProvider();
+		else
+			Common.Log("Permissions/Chat plugin not found, prefix and suffix will not work");
 	}
 
 	public String getPlayerPrefix(Player pl) {
+		if (chat == null)
+			return "";
+
 		return chat.getPlayerPrefix(pl);
 	}
 
 	public String getPlayerSuffix(Player pl) {
+		if (chat == null)
+			return "";
+
 		return chat.getPlayerSuffix(pl);
 	}
 
 	@SuppressWarnings("deprecation")
 	public void takeMoney(String player, double amount) {
-		economy.withdrawPlayer(player, amount);
+		if (economy != null)
+			economy.withdrawPlayer(player, amount);
 	}
 }
